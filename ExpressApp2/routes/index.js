cf03d2175cd05a400cf7a1bfcb467270e1e0b845
jiangwei <jiangwei@dxwt.com>
2017-05-09 10:32:23 +0800

    32423

--- ExpressApp2/routes/index.js
+++ ExpressApp2/routes/index.js
@@ -8,7 +8,7 @@ router.get('/', function (req, res) {
     res.render('index', { title: 'Express' });
 });
 router.get('/a', function (req, res) {
-    res.send("a1344445");
+    res.send("a12344445");
 });
 router.get('/1', function (req, res) {
     res.send("avb1233");
